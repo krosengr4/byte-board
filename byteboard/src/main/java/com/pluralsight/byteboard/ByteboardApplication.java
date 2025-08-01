914c6ddb5038cfaa847d8946de26b9e5bc7efd09
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-01 00:19:21 -0500

    Created base project

---
+++ byteboard/src/main/java/com/pluralsight/byteboard/ByteboardApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.byteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteboardApplication.class, args);
+	}
+
+}
---
+++ byteboard/src/test/java/com/pluralsight/byteboard/ByteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.byteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
