11c8cbc023cb53e88558243dc02f87cf84b062f8
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-05 19:19:18 -0500

    Created class for authentification controller

---
+++ byteboard/src/main/java/com/pluralsight/byteboard/configurations/AuthentificationController.java
@@ -0,0 +1,9 @@
+package com.pluralsight.byteboard.configurations;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class AuthentificationController {
+}
