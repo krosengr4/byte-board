bcbcac57bfa4ae75e2c3d0b2c4a358d702f82061
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-05 21:37:42 -0500

    Created model class for Posts

--- byteboard/src/main/java/com/pluralsight/byteboard/models/Post.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/models/Post.java
@@ -1,4 +1,73 @@
 package com.pluralsight.byteboard.models;
 
+import java.time.LocalDateTime;
+
 public class Post {
+
+	int postId;
+	int userId;
+	String title;
+	String content;
+	String author;
+	LocalDateTime datePosted;
+
+	public Post(int postId, int userId, String title, String content, String author, LocalDateTime datePosted) {
+		this.postId = postId;
+		this.userId = userId;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.datePosted = datePosted;
+	}
+
+	//region Setters and Getters
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public LocalDateTime getDatePosted() {
+		return datePosted;
+	}
+
+	public void setDatePosted(LocalDateTime datePosted) {
+		this.datePosted = datePosted;
+	}
+	//endregion
+
 }
