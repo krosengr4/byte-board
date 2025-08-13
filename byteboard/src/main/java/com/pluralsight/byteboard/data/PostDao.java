3ef153e3d6ca9c98550f47ad5e4b141f3edef225
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 01:43:39 -0500

    Coded interface for PostDao

--- byteboard/src/main/java/com/pluralsight/byteboard/data/PostDao.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/data/PostDao.java
@@ -1,4 +1,21 @@
 package com.pluralsight.byteboard.data;
 
+import com.pluralsight.byteboard.models.Post;
+
+import java.util.List;
+
 public interface PostDao {
+
+	List<Post> getAll();
+
+	Post getById(int postId);
+
+	List<Post> getByUserId(int userId);
+
+	Post add(Post post);
+
+	void update(Post post);
+
+	void delete(int postId);
+
 }
