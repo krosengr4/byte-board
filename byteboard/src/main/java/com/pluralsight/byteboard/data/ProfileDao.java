36a7aefbd793bc391d32a875c2948474f4432295
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 01:44:50 -0500

    Created Dao interface for Profile

--- byteboard/src/main/java/com/pluralsight/byteboard/data/ProfileDao.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/data/ProfileDao.java
@@ -1,4 +1,13 @@
 package com.pluralsight.byteboard.data;
 
+import com.pluralsight.byteboard.models.Profile;
+
 public interface ProfileDao {
+
+	Profile getByUserId(int userId);
+
+	Profile create(Profile profile);
+
+	void update(Profile profile);
+
 }
