862013a357c557e64447d9cf85436e8e3e940a2d
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-18 16:32:59 -0500

    Created method to map a result row to a Profile object and return it

--- byteboard/src/main/java/com/pluralsight/byteboard/data/mysql/MySqlProfileDao.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/data/mysql/MySqlProfileDao.java
@@ -5,6 +5,11 @@ import com.pluralsight.byteboard.models.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
 import javax.sql.DataSource;
 
 @Component
@@ -28,6 +33,19 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao {
 	@Override
 	public void update(Profile profile) {
 
+	}
+
+	private Profile mapRow(ResultSet result) throws SQLException {
+		int userId = result.getInt("user_id");
+		String firstName = result.getString("first_name");
+		String lastName = result.getString("last_name");
+		String email = result.getString("email");
+		String githubLink = result.getString("github_link");
+		String city = result.getString("city");
+		String state = result.getString("state");
+		LocalDate dateRegistered = result.getTimestamp("date_registered").toLocalDateTime().toLocalDate();
+
+		return new Profile(userId, firstName, lastName, email, githubLink, city, state, dateRegistered);
 	}
 
 }
