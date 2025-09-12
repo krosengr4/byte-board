a355c91bad0ab8decdc0edfbf0247512e9e2d1b9
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-12 01:44:16 -0500

    Created method to get all posts in post controller

--- byteboard/src/main/java/com/pluralsight/byteboard/controllers/PostController.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/controllers/PostController.java
@@ -2,10 +2,16 @@ package com.pluralsight.byteboard.controllers;
 
 import com.pluralsight.byteboard.data.PostDao;
 import com.pluralsight.byteboard.data.UserDao;
+import com.pluralsight.byteboard.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,5 +26,24 @@ public class PostController {
 		this.postDao = postDao;
 		this.userDao = userDao;
 	}
+
+	// todo: get all posts
+	@GetMapping("")
+	public List<Post> getAllPosts() {
+		try {
+			return postDao.getAll();
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops, something went wrong...");
+		}
+	}
+	// todo: get all posts from a user with user ID
+
+	// todo: get one post by post ID
+
+	// todo: add a post
+
+	// todo: update a post
+
+	// todo: delete a post
 
 }
