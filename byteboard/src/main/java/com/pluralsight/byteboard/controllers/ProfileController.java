4604895e81a74bd14c17addb2d71633abfed0032
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-12 06:08:22 -0500

    Added controller method to get a users profile

--- byteboard/src/main/java/com/pluralsight/byteboard/controllers/ProfileController.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/controllers/ProfileController.java
@@ -2,10 +2,17 @@ package com.pluralsight.byteboard.controllers;
 
 import com.pluralsight.byteboard.data.ProfileDao;
 import com.pluralsight.byteboard.data.UserDao;
+import com.pluralsight.byteboard.models.Profile;
+import com.pluralsight.byteboard.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 @RestController
 @CrossOrigin
@@ -20,4 +27,20 @@ public class ProfileController {
 		this.profileDao = profileDao;
 		this.userDao = userDao;
 	}
+
+	// todo: get a profile by user ID
+	@GetMapping("")
+	public Profile getByUserId(Principal principal) {
+		try {
+			// Get user ID of the user that is logged in
+			User user = userDao.getByUsername(principal.getName());
+			int userId = user.getId();
+
+			return profileDao.getByUserId(userId);
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops, something went wrong...");
+		}
+	}
+
+	// todo: Update profile method
 }
