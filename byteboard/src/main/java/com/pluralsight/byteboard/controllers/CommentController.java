ee44319b8d50ddc6b6dfee53639b023f675e0c03
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-12 06:46:19 -0500

    Created controller method for getting all comments

--- byteboard/src/main/java/com/pluralsight/byteboard/controllers/CommentController.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/controllers/CommentController.java
@@ -2,10 +2,16 @@ package com.pluralsight.byteboard.controllers;
 
 import com.pluralsight.byteboard.data.CommentDao;
 import com.pluralsight.byteboard.data.UserDao;
+import com.pluralsight.byteboard.models.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,5 +26,25 @@ public class CommentController {
 		this.commentDao = commentDao;
 		this.userDao = userDao;
 	}
+
+	// todo: get all comments
+	@GetMapping("")
+	public List<Comment> getAll() {
+		try {
+			return commentDao.getAll();
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops, something went wrong...");
+		}
+	}
+
+	// todo: get comments on a specific post
+
+	// todo: get one comment by its ID
+
+	// todo: add a comment
+
+	// todo: update a comment
+
+	// todo: delete a comment
 
 }
