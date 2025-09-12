74fcbef3abf9e8f4f0c214983f7380ea38a04382
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-12 06:12:43 -0500

    Created method to update a users profile

--- byteboard/src/main/java/com/pluralsight/byteboard/controllers/ProfileController.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/controllers/ProfileController.java
@@ -6,10 +6,7 @@ import com.pluralsight.byteboard.models.Profile;
 import com.pluralsight.byteboard.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
@@ -43,4 +40,17 @@ public class ProfileController {
 	}
 
 	// todo: Update profile method
+	@PutMapping("/{profileId}")
+	public void updateProfile(@RequestBody Profile profile, Principal principal) {
+		try {
+			// Get user ID of the user that is logged in
+			User user = userDao.getByUsername(principal.getName());
+			int userId = user.getId();
+
+			profile.setUserId(userId);
+			profileDao.update(profile, userId);
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops, something went wrong...");
+		}
+	}
 }
