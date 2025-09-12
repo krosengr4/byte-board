ccf93ae577e9ea90cb4c0319e2339be6a9aeb956
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-11 20:30:25 -0500

    Created controller method for login

--- byteboard/src/main/java/com/pluralsight/byteboard/controllers/AuthentificationController.java
+++ byteboard/src/main/java/com/pluralsight/byteboard/controllers/AuthentificationController.java
@@ -2,10 +2,25 @@ package com.pluralsight.byteboard.controllers;
 
 import com.pluralsight.byteboard.data.ProfileDao;
 import com.pluralsight.byteboard.data.UserDao;
+import com.pluralsight.byteboard.models.User;
+import com.pluralsight.byteboard.models.authentification.LoginDto;
+import com.pluralsight.byteboard.models.authentification.LoginResponseDto;
+import com.pluralsight.byteboard.security.jwt.JWTFilter;
 import com.pluralsight.byteboard.security.jwt.TokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin
@@ -21,5 +36,29 @@ public class AuthentificationController {
 		this.authenticationManagerBuilder = authenticationManagerBuilder;
 		this.userDao = userDao;
 		this.profileDao = profileDao;
+	}
+
+	//POST endpoint = //http://localhost:8080/login
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginDto loginDto) {
+
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = tokenProvider.createToken(authentication, false);
+
+		try {
+			User user = userDao.getByUsername(loginDto.getUsername());
+
+			if(user == null)
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+			return new ResponseEntity<>(new LoginResponseDto(jwt, user), httpHeaders, HttpStatus.OK);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+		}
 	}
 }
